50e1781367220466271bf930ab649b21acc669f9
Tom Jones <250285636@qq.com>
2025-08-28 11:31:32 +0800

    readme

---
+++ readme.mk
@@ -0,0 +1,49 @@
+#用 Gitpod 预览 PHP + MySQL 项目
+
+快速访问
+
+直接打开 Gitpod 预览：
+
+https://gitpod.io/#https://github.com/guomengtao/tp5-shop
+
+指定某个版本（commit 或 tag）：
+
+https://gitpod.io/#https://github.com/guomengtao/tp5-shop/tree/<commit_hash或tag>
+
+示例（tag 方式）：
+
+https://gitpod.io/#https://github.com/guomengtao/tp5-shop/tree/v1.0
+
+示例（commit_hash 方式）：
+
+https://gitpod.io/#https://github.com/guomengtao/tp5-shop/tree/3da108
+或
+https://gitpod.io/#https://github.com/guomengtao/tp5-shop/tree/3da10809c8ccdd6ac24162f80f3286ebd526c097
+
+
+
+切换版本（命令行）
+
+进入 Gitpod 后，可在终端执行：
+
+# 切换到指定 commit
+git checkout 3da10809c8ccdd6ac24162f80f3286ebd526c097
+
+# 或切换到指定 tag
+git checkout v1.0
+
+预览运行
+
+进入项目目录后：
+
+# 启动 PHP 内置服务器
+php -S 0.0.0.0:8080
+
+# 数据库已自动导入 db_init.sql
+
+打开浏览器访问：
+
+https://<你的 Gitpod 工作区域>.ws-us121.gitpod.io/
+
+
+ 
